3aacbc299330efb0d507397a117bafe11820bfb9
MrKhoi <92254546+DwngKhoi@users.noreply.github.com>
2024-01-15 09:37:44 +0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -857,7 +857,7 @@ function KLib:MakeWindow(WindowConfig)
 				ButtonConfig = ButtonConfig or {}
 				ButtonConfig.Name = ButtonConfig.Name or "Button"
 				ButtonConfig.Callback = ButtonConfig.Callback or function() end
-				ButtonConfig.Icon = ButtonConfig.Icon or "rbxassetid://3944703587"
+				ButtonConfig.Icon = ButtonConfig.Icon or "rbxassetid://2418686949"
 
 				local Button = {}
 
